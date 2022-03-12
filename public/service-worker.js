5fddfe89ed966506d614baba49e9334a621cf3b6
Wenwen Tian <wwtian9@gmail.com>
2022-03-12 15:46:46 -0700

    activate service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,7 @@
-const CACHE_NAME = "BudgetTracker-v1";
+const APP_PREFIX = "BudgetTracker-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
+
 const FILES_TO_CACHE = [
   "./index.html",
   "./manifest.json",
@@ -9,12 +12,36 @@ const FILES_TO_CACHE = [
   "./icons/icon-512x512.png",
 ];
 
-// Cache resources, install service worker
+// Install service worker. Cache resources,
 self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       console.log("installing cache : " + CACHE_NAME);
       return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// Activate the Service Worker. Delete outdated caches
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create keeplist
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      // add current cache name to keeplist
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log("deleting cache : " + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
     })
   );
 });
