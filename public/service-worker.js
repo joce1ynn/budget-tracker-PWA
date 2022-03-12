3218266173c01c70febae04987909866b2fdc3a7
Wenwen Tian <wwtian9@gmail.com>
2022-03-12 15:35:32 -0700

    install service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,20 @@
+const CACHE_NAME = "BudgetTracker-v1";
+const FILES_TO_CACHE = [
+  "./index.html",
+  "./manifest.json",
+  "./js/idb.js",
+  "./js/index.js",
+  "./css/styles.css",
+  "./icons/icon-192x192.png",
+  "./icons/icon-512x512.png",
+];
 
-
-// Offline entries should be added to the tracker when the application is brought back online.
+// Cache resources, install service worker
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("installing cache : " + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
